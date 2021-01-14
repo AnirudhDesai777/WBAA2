cf42ca324a2ad79726b6abac06b022aefda5e4e1
AnirudhDesai777 <57042355+AnirudhDesai777@users.noreply.github.com>
2021-01-14 13:36:18 +0530

    Added display_information.html

--- backlog/backlog/urls.py
+++ backlog/backlog/urls.py
@@ -19,5 +19,7 @@ from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',views.index, name="index:")
+    path('', views.index, name="index:"),
+    path('otp', views.otp, name="otp"),
+    path('display', views.display_information, name="display")
 ]
--- backlog/backlog/views.py
+++ backlog/backlog/views.py
@@ -4,3 +4,11 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'index.html')
+
+
+def otp(request):
+    return render(request, 'otp.html')
+
+
+def display_information(request):
+    return render(request, 'display_information.html')
