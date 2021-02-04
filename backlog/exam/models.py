731c7afb8cd35daf1b3b60831ba972d83406e99e
anupam-ahi <anupam.gollapalli@gmail.com>
2021-02-04 21:22:58 +0530

    created a database that stores the student details

--- backlog/exam/models.py
+++ backlog/exam/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+
+class student_details(models.Model):
+    USN = models.CharField(max_length=11)
+    MOBILE = models.IntegerField()
+    EMAIL = models.EmailField(max_length=34)
--- backlog/exam/views.py
+++ backlog/exam/views.py
@@ -5,9 +5,19 @@ from django.http import HttpResponse
 from django.shortcuts import render
 import requests
 import json
+from .models import student_details
 
 
 def index1(request):
+    student_usn = request.POST.get('USN')
+    student_mobile = request.POST.get('MOBILE')
+    student_email = request.POST.get('EMAIL')
+
+    post = student_details()
+    post.USN = student_usn
+    post.MOBILE = student_mobile
+    post.EMAIL = student_email
+    post.save()
 
     print(request.POST.get('USN'))
     print(request.POST.get('MOBILE'))
