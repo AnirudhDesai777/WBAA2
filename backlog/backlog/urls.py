2285eb09cef4583c454dbd04b875ca634fb71893
anupam-ahi <anupam.gollapalli@gmail.com>
2021-02-04 20:16:51 +0530

    i fixed a bug regarding type of request#

--- backlog/backlog/urls.py
+++ backlog/backlog/urls.py
@@ -21,5 +21,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.index1, name="index1"),
     path('otp', views.otp, name="otp"),
+
     path('display', views.display_information, name="display")
 ]
--- backlog/backlog/views.py
+++ backlog/backlog/views.py
@@ -1,9 +1,20 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+import requests
+import json
 
 
 def index1(request):
+    # if request.method == 'POST':
+    # regx1 = request.POST.get['regx1']
+    print(request.POST.get('USN'))
+    print(request.POST.get('MOBILE'))
+    print(request.POST.get('EMAIL'))
+    # usn = request.POST['usn']
+    # mobile = request.POST['mobile']
+    # print(mobile)
     return render(request, 'index1.html')
+    # return render(request, 'index1.html', {'usn': regx1})
 
 
 def otp(request):
@@ -12,3 +23,6 @@ def otp(request):
 
 def display_information(request):
     return render(request, 'display_information.html')
+
+
+# def get_data(request):
