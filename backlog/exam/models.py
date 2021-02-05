1480b21f4ea0b797c979c15a43ca37616a6c3bb2
archana2420 <rajivishnu47@gmail.com>
2021-02-05 10:57:22 +0530

    Archana's edit(fixed table)

--- backlog/exam/models.py
+++ backlog/exam/models.py
@@ -4,6 +4,6 @@ from django.db import models
 
 
 class student_details(models.Model):
-    USN = models.CharField(max_length=11)
-    MOBILE = models.IntegerField()
-    EMAIL = models.EmailField(max_length=34)
+    USN = models.CharField(max_length=11,null=True,blank=True)
+    MOBILE = models.IntegerField(null=True,blank=True)
+    EMAIL = models.EmailField(max_length=32,null=True,blank=True)
